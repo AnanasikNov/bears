e8f56d7f0d651ca549f9ab9aff02caef2daaaddc
unknown <novinkinaa.i20@gmail.com>
2022-06-26 01:05:15 +0300

    глобальная переменная и сессия куки

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var heroes = require('./routes/heroes');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/all')
+var session = require('express-session');
+var MongoStore = require('connect-mongo');
+var Hero = require("./models/hero").Hero
 
 var app = express();
 
 // view engine setup
-app.engine('ejs',require('ejs-locals'));
+app.engine('ejs', require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use('/heroes', heroes);
@@ -24,23 +27,36 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(session({
+  secret: "all",
+  cookie: { maxAge: 60 * 1000 },
+  store: MongoStore.create({ mongoUrl: 'mongodb://localhost/all' })
+}))
+
+app.use(function (req, res, next) {
+  req.session.counter = req.session.counter + 1 || 1
+  next()
+})
+
+app.use(require("./middleware/createMenu.js"))
+
+app.use('/', routes);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {title:'Ошибка'});
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,13 @@
-var express = require('express')
-var router = express.Router()
-var Hero = require("../models/hero").Hero
-
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
-        res.render('index', {
-                                title: 'Bears',
-                                menu: menu
-                            });
-    })
-
+router.get('/', function (req, res, next) {
+  req.session.greeting = "Hi!!!!"
+  res.render('index', {
+    title: 'Bears',
+    counter: req.session.counter
+  });
 });
 
 module.exports = router;
